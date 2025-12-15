5997696b8ca43e2e989548b98ed87b5c7aa82f60
Kher92 <152818859+Kher92@users.noreply.github.com>
2025-12-15 15:52:08 +0100

    Refactor Streamlit app layout and title
    Updated page title and added an image to the layout.

--- beispiel.py
+++ beispiel.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import io
 st.set_page_config(
-    page_title="Probe",
+    page_title="Auszählung",
     layout="wide",
     page_icon="📊"
 )
@@ -13,7 +13,9 @@ excel_files = [
     "https://github.com/Kher92/KS_FIles/blob/main/2025-12-01-017_KS_3421.xlsx",
     "https://github.com/Kher92/KS_FIles/blob/main/2025-12-02-035_KS_2390.xlsx"
 ]
-
+col1, col2, col3 = st.columns([1,6,1]) # Adjust column ratios as needed
+with col2:
+    st.image("gemini.png")
 selected_file_url = st.selectbox("Wähle die Datei aus", excel_files)
 
 file_url = selected_file_url.replace("github.com", "raw.githubusercontent.com").replace("/blob/", "/")
