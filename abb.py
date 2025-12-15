4ebbfcd5eac38c0687bdd6f3477dc159f33af52c
Kher92 <152818859+Kher92@users.noreply.github.com>
2025-12-15 12:56:42 +0100

    Add files via upload

---
+++ abb.py
@@ -0,0 +1,74 @@
+import streamlit as st
+import pandas as pd
+import os
+from io import BytesIO
+
+files_in = r"C:\Users\ks\Desktop\excelFiles"
+
+st.set_page_config(
+    page_title="Probe",
+    layout="wide",
+    page_icon="📊"
+)
+
+excel_files = [
+    f for f in os.listdir(files_in)
+    if f.lower().endswith((".xlsx", ".xls"))
+]
+
+selected_file = st.selectbox(
+    "Wähle die Datei aus",
+    excel_files
+)
+
+st.title(f"📊 Auftrag: {selected_file}")
+
+file_path = os.path.join(files_in, selected_file)
+
+@st.cache_data
+def load_data(path):
+    df = pd.read_excel(path)
+    return df
+
+df = load_data(file_path)
+df = df.dropna(how="all")
+
+st.subheader("📋 Data Preview")
+st.dataframe(df, use_container_width=True)
+
+st.subheader("📈 Pivot Table Builder")
+cols = df.columns.tolist()
+
+# rows = st.multiselect("Rows", cols)
+# columns = st.multiselect("Columns", cols)
+# values = st.selectbox("Values", cols)
+
+# if rows and columns and values:
+#     pivot = pd.pivot_table(
+#         df,
+#         index=rows,
+#         columns=columns,
+#         values=values,
+#         aggfunc="count",
+#         fill_value=0
+#     )
+#     st.dataframe(pivot, use_container_width=True)
+
+# st.download_button(
+#     label="⬇️ Download data as excel",
+#     data=df.to_excel(index=False).encode("latin1"),
+#     file_name=f"{selected_file}.xlsx",
+#     mime="text/xlsx"
+# )
+
+
+output = BytesIO()
+df.to_excel(output, index=False, engine="openpyxl")
+output.seek(0)
+
+st.download_button(
+    label="⬇️ Download data as Excel",
+    data=output,
+    file_name=f"{selected_file}",
+    mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+)
